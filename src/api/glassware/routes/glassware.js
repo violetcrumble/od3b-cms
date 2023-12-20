49f811bfed99ed6a829cfecaa669867a898d2734
Bonnie <onedrinkthreebars@gmail.com>
2023-12-20 09:52:02 -0500

    removing glassware and adding fields to recipe

--- src/api/glassware/routes/glassware.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * glassware router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::glassware.glassware');
--- src/api/glassware/services/glassware.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * glassware service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::glassware.glassware');
